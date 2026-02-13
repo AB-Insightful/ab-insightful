b4254cea7eeebde7e910b2ab156f762c26a465a2
ChurchDuck1 <bchurch2@csus.edu>
2026-02-12 20:00:50 -0800

    all necessary buttons added

--- app/routes/app.settings.jsx
+++ app/routes/app.settings.jsx
@@ -1,9 +1,9 @@
 export default function Reports() {
     return (
         <s-page heading="App Settings">
-            <s-secton>
-                <s-paragraph>An empty card</s-paragraph>
-            </s-secton>
+            <s-section heading="Support & Doccumentation">
+                <s-link href="/app/help">How To's and Support</s-link>
+            </s-section>
         </s-page>
     );
 }

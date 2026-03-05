3ebf5ab2b7e68bf7ee89b47399fa675ab3681a05
Ryan Martinez <ryanmartinez2021@gmail.com>
2026-03-04 18:29:12 -0800

    fixed secret generation script

--- scripts/ops/windows/generate-cron-secret.ps1
+++ scripts/ops/windows/generate-cron-secret.ps1
@@ -1,4 +1,4 @@
 $secret = node .\scripts\ops\windows\execute-generation.cjs
 echo "New Cron Secret: ${secret}"
 fly secrets set -a ab-insightful CRON_SECRET=$secret
-fly secrets show
+fly secrets list
